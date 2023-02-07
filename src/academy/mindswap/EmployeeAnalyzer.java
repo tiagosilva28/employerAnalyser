afa02a18b97a2f81b421906459db08ac4f366196
tiagosilva28 <122462180+tiagosilva28@users.noreply.github.com>
2023-02-07 12:43:58 +0000

    Add files via upload

---
+++ src/academy/mindswap/EmployeeAnalyzer.java
@@ -0,0 +1,39 @@
+package academy.mindswap;
+
+import academy.mindswap.employee.Employee;
+
+import java.time.Year;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class EmployeeAnalyzer {
+
+    public long countEmployees(List<Employee> employees, int years) {
+
+        return 0;
+    }
+
+    public List<String> findEmployeeBySalary(List<Employee> employees, int salary) {
+        return new ArrayList<>();
+    }
+
+    public List<Employee> findOldestEmployees(List<Employee> employees, int numberOfEmployees) {
+        return  new ArrayList<>();
+    }
+
+    public Optional<Employee> findFirstEmployeeByAge(List<Employee> employees, int age) {
+
+
+        return Optional.empty();
+    }
+
+    public Double findAverageSalary(List<Employee> employees) {
+        return 0.0;
+    }
+
+    public List<String> findCommonNames(List<Employee> firstDepartment, List<Employee> secondDepartment) {
+
+
+        return new ArrayList<>();
+    }
+}
