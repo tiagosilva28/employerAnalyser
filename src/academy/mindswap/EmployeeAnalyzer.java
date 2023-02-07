722382d2e7627a476c9d51cf9ce55255c1ddf2d7
tiaogosilva28 <tiagosilva28@live.com.pt>
2023-02-07 19:22:13 +0000

    ALL DONE !!!!!!!!

--- src/academy/mindswap/EmployeeAnalyzer.java
+++ src/academy/mindswap/EmployeeAnalyzer.java
@@ -63,7 +63,6 @@ public class EmployeeAnalyzer {
 
     public List<String> findCommonNames(List<Employee> firstDepartment, List<Employee> secondDepartment) {
         //counter para os nomes e comparar o que aparece mais
-        //aa
 
         List<String> result = firstDepartment.stream()
                 .filter(two -> secondDepartment.stream()
