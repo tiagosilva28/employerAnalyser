4fd408397b9500921a6246e8089ed7dd8f0039bb
johnDoCodigo <122378532+johnDoCodigo@users.noreply.github.com>
2023-02-07 16:08:17 +0000

    2nd amends countemployees

--- src/academy/mindswap/EmployeeAnalyzer.java
+++ src/academy/mindswap/EmployeeAnalyzer.java
@@ -5,15 +5,29 @@ import academy.mindswap.employee.Employee;
 import java.time.Year;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EmployeeAnalyzer {
 
     public long countEmployees(List<Employee> employees, int years) {
+        Year year = Year.now();
+        int currentYear = year.getValue();
+        Stream<Employee> employeeStream = employees.stream();
 
-        return 0;
+                long count = employeeStream
+                .filter(employee -> (currentYear - employee.getStartingYear())  == years)
+                .count();
+        return count;
     }
 
     public List<String> findEmployeeBySalary(List<Employee> employees, int salary) {
+
+        Stream<Employee> employeeStream = employees.stream();
+        Arrays.stream(employeeStream)
+
+
+
+
         return new ArrayList<>();
     }
 
